388f7c50d3649e64c357a7bb5c74cfd60b793481
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-09-27 22:10:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import streamlit as st
 
+st.write("""
+Mon premier est une lettre de l'alphabet, souvent associée à une question ou à une incertitude.
+Mon deuxième est un mot désignant un élément de la nature qui est essentiel à la vie, souvent associé à la transparence et à la fluidité.
+Mon tout est un concept philosophique et mathématique, souvent considéré comme fondamental dans la logique et les sciences.
+mon dernier ne peut etre stocké que sur 8 bits + 1""")
