9e52a000b6b905d32286ab30d0a2d42f0e29df1c
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-09-27 22:14:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import os
 
 st.write("""
 Mon premier est une lettre de l'alphabet, souvent associée à une question ou à une incertitude.
 Mon deuxième est un mot désignant un élément de la nature qui est essentiel à la vie, souvent associé à la transparence et à la fluidité.
 Mon tout est un concept philosophique et mathématique, souvent considéré comme fondamental dans la logique et les sciences.
 mon dernier ne peut etre stocké que sur 8 bits + 1""")
+
+if st.input_text(label="enter the code") == os.environ["code"]:
+  st.write("92006fa632c3777478a924d3f21cd9eb1a21e4164bdbfa90490c9e77c9ae22e0")
+else:
+  st.write("try other things !")
