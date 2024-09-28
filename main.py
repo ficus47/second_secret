9562da27310c88a1333ba0214a8eb763336ba60c
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-09-28 14:02:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ mon dernier ne peut etre stocké que sur 8 bits + 1""")
 
 if st.text_input(label="enter the code") == os.environ["code"]:
   st.write("92006fa632c3777478a924d3f21cd9eb1a21e4164bdbfa90490c9e77c9ae22e0")
+  st.write("114 145 040 114 151 350 166 162 145 040 145 164 040 154 141 040 124 157 162 164 165 145")
 else:
   st.write("try other things !")
